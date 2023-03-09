9b5c61f4320082469e32c3bfd92179e8fb9085e4
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-03-09 10:34:38 +0000

    added coupons

--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from coupons.forms import CouponApplyForm
 
 # Create your views here.
 
@@ -34,5 +35,6 @@ def cart_detail(request):
         item['update_quantity_form'] = CartAddProductForm(initial={
             'quantity': item['quantity'],
             'override': True})
-    context = {'cart': cart}
+    coupon_apply_form = CouponApplyForm()
+    context = {'cart': cart, 'coupon_apply_form': coupon_apply_form}
     return render(request, template_name, context)
